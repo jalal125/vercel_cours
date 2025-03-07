4176d5cb896fd2aa59541718bf6f3ee7bb398231
Jalal125 <jalaleddinebouchrit@gmail.com>
2025-03-07 12:06:18 +0100

    Ajout de l'intégration Supabase

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+const { $supabase } = useNuxtApp()
+const { data } = await $supabase.from('users').select('*')
+</script>
+
+<template>
+  <div>
+    <h1>Liste des utilisateurs</h1>
+    <ul>
+      <li v-for="user in data" :key="user.id">
+        {{ user.name }}
+      </li>
+    </ul>
+  </div>
+</template>
---
+++ plugins/supabase.js
@@ -0,0 +1,13 @@
+import { createClient } from '@supabase/supabase-js'
+
+export default defineNuxtPlugin(() => {
+  const supabaseUrl = useRuntimeConfig().public.supabaseUrl
+  const supabaseKey = useRuntimeConfig().public.supabaseAnonKey
+  const supabase = createClient(supabaseUrl, supabaseKey)
+
+  return {
+    provide: {
+      supabase
+    }
+  }
+})
